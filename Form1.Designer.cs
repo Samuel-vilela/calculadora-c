eb7d2cd1d1303d9fe20482513d724fea63819676
samuel.varaujo <samuel.varaujo@AME0613343W10-1>
2024-03-20 21:55:40 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,370 @@
+﻿namespace teste
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btn_porcen = new System.Windows.Forms.Button();
+            this.btn_ce = new System.Windows.Forms.Button();
+            this.btn_c = new System.Windows.Forms.Button();
+            this.btn_excluir = new System.Windows.Forms.Button();
+            this.btn_ = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.btn_div = new System.Windows.Forms.Button();
+            this.btn_vezes = new System.Windows.Forms.Button();
+            this.btn_sete = new System.Windows.Forms.Button();
+            this.btn_quatro = new System.Windows.Forms.Button();
+            this.btn_um = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.btn_oito = new System.Windows.Forms.Button();
+            this.btn_cinco = new System.Windows.Forms.Button();
+            this.btn_dois = new System.Windows.Forms.Button();
+            this.btn_zero = new System.Windows.Forms.Button();
+            this.btn_nove = new System.Windows.Forms.Button();
+            this.btn_seis = new System.Windows.Forms.Button();
+            this.btn_tres = new System.Windows.Forms.Button();
+            this.btn_virgula = new System.Windows.Forms.Button();
+            this.btn_subtracao = new System.Windows.Forms.Button();
+            this.btn_adicao = new System.Windows.Forms.Button();
+            this.btn_resultado = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_porcen
+            // 
+            this.btn_porcen.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.btn_porcen.Location = new System.Drawing.Point(12, 172);
+            this.btn_porcen.Name = "btn_porcen";
+            this.btn_porcen.Size = new System.Drawing.Size(107, 94);
+            this.btn_porcen.TabIndex = 0;
+            this.btn_porcen.Text = "%";
+            this.btn_porcen.UseVisualStyleBackColor = false;
+            this.btn_porcen.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn_ce
+            // 
+            this.btn_ce.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.btn_ce.Location = new System.Drawing.Point(125, 172);
+            this.btn_ce.Name = "btn_ce";
+            this.btn_ce.Size = new System.Drawing.Size(108, 94);
+            this.btn_ce.TabIndex = 1;
+            this.btn_ce.Text = "ce";
+            this.btn_ce.UseVisualStyleBackColor = false;
+            // 
+            // btn_c
+            // 
+            this.btn_c.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.btn_c.Location = new System.Drawing.Point(239, 172);
+            this.btn_c.Name = "btn_c";
+            this.btn_c.Size = new System.Drawing.Size(112, 94);
+            this.btn_c.TabIndex = 2;
+            this.btn_c.Text = "c";
+            this.btn_c.UseVisualStyleBackColor = false;
+            // 
+            // btn_excluir
+            // 
+            this.btn_excluir.BackColor = System.Drawing.Color.IndianRed;
+            this.btn_excluir.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn_excluir.BackgroundImage")));
+            this.btn_excluir.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btn_excluir.Location = new System.Drawing.Point(360, 172);
+            this.btn_excluir.Name = "btn_excluir";
+            this.btn_excluir.Size = new System.Drawing.Size(135, 94);
+            this.btn_excluir.TabIndex = 3;
+            this.btn_excluir.UseVisualStyleBackColor = false;
+            this.btn_excluir.Click += new System.EventHandler(this.btn_excluir_Click);
+            // 
+            // btn_
+            // 
+            this.btn_.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn_.Location = new System.Drawing.Point(12, 272);
+            this.btn_.Name = "btn_";
+            this.btn_.Size = new System.Drawing.Size(107, 80);
+            this.btn_.TabIndex = 4;
+            this.btn_.Text = "¹/x";
+            this.btn_.UseVisualStyleBackColor = false;
+            this.btn_.Click += new System.EventHandler(this.btn__Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button6.Location = new System.Drawing.Point(125, 272);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(108, 80);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "x²";
+            this.button6.UseVisualStyleBackColor = false;
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button7.Location = new System.Drawing.Point(239, 272);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(115, 80);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "²vx";
+            this.button7.UseVisualStyleBackColor = false;
+            // 
+            // btn_div
+            // 
+            this.btn_div.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.btn_div.Location = new System.Drawing.Point(357, 272);
+            this.btn_div.Name = "btn_div";
+            this.btn_div.Size = new System.Drawing.Size(135, 80);
+            this.btn_div.TabIndex = 7;
+            this.btn_div.Text = "/";
+            this.btn_div.UseVisualStyleBackColor = false;
+            // 
+            // btn_vezes
+            // 
+            this.btn_vezes.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.btn_vezes.Location = new System.Drawing.Point(360, 358);
+            this.btn_vezes.Name = "btn_vezes";
+            this.btn_vezes.Size = new System.Drawing.Size(135, 76);
+            this.btn_vezes.TabIndex = 8;
+            this.btn_vezes.Text = "x";
+            this.btn_vezes.UseVisualStyleBackColor = false;
+            this.btn_vezes.Click += new System.EventHandler(this.btn_vezes_Click);
+            // 
+            // btn_sete
+            // 
+            this.btn_sete.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn_sete.Location = new System.Drawing.Point(12, 358);
+            this.btn_sete.Name = "btn_sete";
+            this.btn_sete.Size = new System.Drawing.Size(107, 76);
+            this.btn_sete.TabIndex = 9;
+            this.btn_sete.Text = "7";
+            this.btn_sete.UseVisualStyleBackColor = false;
+            this.btn_sete.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // btn_quatro
+            // 
+            this.btn_quatro.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn_quatro.Location = new System.Drawing.Point(12, 441);
+            this.btn_quatro.Name = "btn_quatro";
+            this.btn_quatro.Size = new System.Drawing.Size(107, 81);
+            this.btn_quatro.TabIndex = 10;
+            this.btn_quatro.Text = "4";
+            this.btn_quatro.UseVisualStyleBackColor = false;
+            // 
+            // btn_um
+            // 
+            this.btn_um.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn_um.Location = new System.Drawing.Point(12, 529);
+            this.btn_um.Name = "btn_um";
+            this.btn_um.Size = new System.Drawing.Size(107, 70);
+            this.btn_um.TabIndex = 11;
+            this.btn_um.Text = "1";
+            this.btn_um.UseVisualStyleBackColor = false;
+            this.btn_um.Click += new System.EventHandler(this.btn_um_Click);
+            // 
+            // button13
+            // 
+            this.button13.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.button13.Location = new System.Drawing.Point(12, 605);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(107, 71);
+            this.button13.TabIndex = 12;
+            this.button13.Text = "+/-";
+            this.button13.UseVisualStyleBackColor = false;
+            // 
+            // btn_oito
+            // 
+            this.btn_oito.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn_oito.Location = new System.Drawing.Point(125, 359);
+            this.btn_oito.Name = "btn_oito";
+            this.btn_oito.Size = new System.Drawing.Size(108, 75);
+            this.btn_oito.TabIndex = 13;
+            this.btn_oito.Text = "8";
+            this.btn_oito.UseVisualStyleBackColor = false;
+            this.btn_oito.Click += new System.EventHandler(this.button14_Click);
+            // 
+            // btn_cinco
+            // 
+            this.btn_cinco.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn_cinco.Location = new System.Drawing.Point(125, 440);
+            this.btn_cinco.Name = "btn_cinco";
+            this.btn_cinco.Size = new System.Drawing.Size(108, 82);
+            this.btn_cinco.TabIndex = 14;
+            this.btn_cinco.Text = "5";
+            this.btn_cinco.UseVisualStyleBackColor = false;
+            // 
+            // btn_dois
+            // 
+            this.btn_dois.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn_dois.Location = new System.Drawing.Point(125, 529);
+            this.btn_dois.Name = "btn_dois";
+            this.btn_dois.Size = new System.Drawing.Size(108, 70);
+            this.btn_dois.TabIndex = 15;
+            this.btn_dois.Text = "2";
+            this.btn_dois.UseVisualStyleBackColor = false;
+            // 
+            // btn_zero
+            // 
+            this.btn_zero.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn_zero.Location = new System.Drawing.Point(125, 605);
+            this.btn_zero.Name = "btn_zero";
+            this.btn_zero.Size = new System.Drawing.Size(108, 70);
+            this.btn_zero.TabIndex = 16;
+            this.btn_zero.Text = "0";
+            this.btn_zero.UseVisualStyleBackColor = false;
+            // 
+            // btn_nove
+            // 
+            this.btn_nove.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn_nove.Location = new System.Drawing.Point(239, 358);
+            this.btn_nove.Name = "btn_nove";
+            this.btn_nove.Size = new System.Drawing.Size(115, 76);
+            this.btn_nove.TabIndex = 17;
+            this.btn_nove.Text = "9";
+            this.btn_nove.UseVisualStyleBackColor = false;
+            this.btn_nove.Click += new System.EventHandler(this.button18_Click);
+            // 
+            // btn_seis
+            // 
+            this.btn_seis.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn_seis.Location = new System.Drawing.Point(239, 440);
+            this.btn_seis.Name = "btn_seis";
+            this.btn_seis.Size = new System.Drawing.Size(115, 81);
+            this.btn_seis.TabIndex = 18;
+            this.btn_seis.Text = "6";
+            this.btn_seis.UseVisualStyleBackColor = false;
+            // 
+            // btn_tres
+            // 
+            this.btn_tres.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn_tres.Location = new System.Drawing.Point(239, 527);
+            this.btn_tres.Name = "btn_tres";
+            this.btn_tres.Size = new System.Drawing.Size(115, 70);
+            this.btn_tres.TabIndex = 19;
+            this.btn_tres.Text = "3";
+            this.btn_tres.UseVisualStyleBackColor = false;
+            // 
+            // btn_virgula
+            // 
+            this.btn_virgula.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btn_virgula.Location = new System.Drawing.Point(239, 603);
+            this.btn_virgula.Name = "btn_virgula";
+            this.btn_virgula.Size = new System.Drawing.Size(115, 70);
+            this.btn_virgula.TabIndex = 20;
+            this.btn_virgula.Text = ",";
+            this.btn_virgula.UseVisualStyleBackColor = false;
+            // 
+            // btn_subtracao
+            // 
+            this.btn_subtracao.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.btn_subtracao.Location = new System.Drawing.Point(360, 440);
+            this.btn_subtracao.Name = "btn_subtracao";
+            this.btn_subtracao.Size = new System.Drawing.Size(135, 81);
+            this.btn_subtracao.TabIndex = 21;
+            this.btn_subtracao.Text = "_";
+            this.btn_subtracao.UseVisualStyleBackColor = false;
+            // 
+            // btn_adicao
+            // 
+            this.btn_adicao.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.btn_adicao.Location = new System.Drawing.Point(360, 527);
+            this.btn_adicao.Name = "btn_adicao";
+            this.btn_adicao.Size = new System.Drawing.Size(135, 70);
+            this.btn_adicao.TabIndex = 22;
+            this.btn_adicao.Text = "+";
+            this.btn_adicao.UseVisualStyleBackColor = false;
+            // 
+            // btn_resultado
+            // 
+            this.btn_resultado.BackColor = System.Drawing.Color.Blue;
+            this.btn_resultado.Location = new System.Drawing.Point(360, 603);
+            this.btn_resultado.Name = "btn_resultado";
+            this.btn_resultado.Size = new System.Drawing.Size(135, 70);
+            this.btn_resultado.TabIndex = 23;
+            this.btn_resultado.Text = "=";
+            this.btn_resultado.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(512, 718);
+            this.Controls.Add(this.btn_resultado);
+            this.Controls.Add(this.btn_adicao);
+            this.Controls.Add(this.btn_subtracao);
+            this.Controls.Add(this.btn_virgula);
+            this.Controls.Add(this.btn_tres);
+            this.Controls.Add(this.btn_seis);
+            this.Controls.Add(this.btn_nove);
+            this.Controls.Add(this.btn_zero);
+            this.Controls.Add(this.btn_dois);
+            this.Controls.Add(this.btn_cinco);
+            this.Controls.Add(this.btn_oito);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.btn_um);
+            this.Controls.Add(this.btn_quatro);
+            this.Controls.Add(this.btn_sete);
+            this.Controls.Add(this.btn_vezes);
+            this.Controls.Add(this.btn_div);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.btn_);
+            this.Controls.Add(this.btn_excluir);
+            this.Controls.Add(this.btn_c);
+            this.Controls.Add(this.btn_ce);
+            this.Controls.Add(this.btn_porcen);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_porcen;
+        private System.Windows.Forms.Button btn_ce;
+        private System.Windows.Forms.Button btn_c;
+        private System.Windows.Forms.Button btn_excluir;
+        private System.Windows.Forms.Button btn_;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button btn_div;
+        private System.Windows.Forms.Button btn_vezes;
+        private System.Windows.Forms.Button btn_sete;
+        private System.Windows.Forms.Button btn_quatro;
+        private System.Windows.Forms.Button btn_um;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button btn_oito;
+        private System.Windows.Forms.Button btn_cinco;
+        private System.Windows.Forms.Button btn_dois;
+        private System.Windows.Forms.Button btn_zero;
+        private System.Windows.Forms.Button btn_nove;
+        private System.Windows.Forms.Button btn_seis;
+        private System.Windows.Forms.Button btn_tres;
+        private System.Windows.Forms.Button btn_virgula;
+        private System.Windows.Forms.Button btn_subtracao;
+        private System.Windows.Forms.Button btn_adicao;
+        private System.Windows.Forms.Button btn_resultado;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace teste
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_vezes_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_um_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn__Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_excluir_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
